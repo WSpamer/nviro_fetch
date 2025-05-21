7b52fe226d8b1f5a706cf29da8fbfad92c7c0f53
WSpamer <spamer24310@gmail.com>
2025-05-21 09:35:35 +0200

    Added testing for fetching readings

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from src.nviro_fetch.auth import authenticate
+from nviro_fetch.fetch import fetch_devices, fetch_sensor_readings, fetch_device_sensors
+from nviro_fetch.data import get_sensor_readings, extract_readings
 
-token = authenticate()
-print(token)
+
+def run():
+    token = authenticate()
+    devices = fetch_devices(token)
+    SAMPLE_START_DATE = "2025-03-01T00:00:00"  # Start date in ISO8601 format
+    SAMPLE_END_DATE = "2025-03-24T23:59:59"
+
+    device = [device for device in devices if device["device_group"] == "Goedhals"][0]
+    devEui = device["devEui"]
+    readings_data = fetch_sensor_readings(
+        token,
+        devEui,
+        SAMPLE_START_DATE,
+        SAMPLE_END_DATE,
+    )
+    readings = get_sensor_readings(readings_data)
+    print(readings[0])
+
+
+if __name__ == "__main__":
+    run()
