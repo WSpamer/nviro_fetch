60ee0119ccf76a22377fc1fc9ac42456ab59b089
WSpamer <spamer24310@gmail.com>
2025-05-23 13:19:29 +0200

    Replaced print statements with logging

--- src/nviro_fetch/auth.py
+++ src/nviro_fetch/auth.py
@@ -1,11 +1,17 @@
-import requests
 import json
-import time
-import sys
 import os
+import sys
+import time
+
 import dotenv
+import requests
+from loguru import logger
 
 __all__ = ["authenticate"]
+dotenv.load_dotenv()
+path = os.environ.get("PROJECT_PATH")
+logger.add(f"{path}/logs/auth.log")
+# logger_auth = logger.bind(name="auth")
 
 
 # Logging function (minimal logging)
@@ -23,14 +29,13 @@ def parse_json(response_body):
         sys.exit(1)
 
 
-def fetch_login(print_response=False):
+def fetch_login():
     JWT_ENDPOINT = "https://ant.nvirosense.com/api/v1/login"
 
     dotenv.load_dotenv()
     NVIRO_USERNAME = os.environ.get("NVIRO_USERNAME")
     NVIRO_PASSWORD = os.environ.get("NVIRO_PASSWORD")
-    if print_response:
-        print(f"[INFO] Authenticating with {JWT_ENDPOINT}...")
+    logger.info(f"Authenticating with {JWT_ENDPOINT}...")
     headers = {"Content-Type": "application/json"}
     payload = {
         "user": {
@@ -43,43 +48,40 @@ def fetch_login(print_response=False):
     return response
 
 
-def authenticate(print_response=False):
+def authenticate():
 
     retries = 0
     MAX_RETRIES = 3
     RETRY_DELAY = 2  # seconds
     while True:
         try:
-            response = fetch_login(print_response=print_response)
+            response = fetch_login()
             if response.status_code == 200:
                 json_response = parse_json(response.text)
 
                 jwt_token = json_response.get("token")
-                if print_response:
-                    print(json_response)
-                    print(jwt_token)
+                logger.info(json_response)
                 if not jwt_token:
                     auth_header = response.headers.get("Authorization")
                     if auth_header:
                         parts = auth_header.split(" ")
                         if len(parts) >= 2:
                             jwt_token = parts[-1]
-                            if print_response:
-                                print(jwt_token)
+                            logger.info(f"Token: {jwt_token}")
                 if jwt_token:
-                    print("[SUCCESS] Authentication successful! Token received.")
+                    logger.success("Authentication successful! Token received.")
                     return jwt_token
                 else:
-                    print("[ERROR] Failed to extract token from response!")
+                    logger.error("Failed to extract token from response!")
                     sys.exit(1)
             else:
-                print("[ERROR] Authentication failed! Retrying...")
+                logger.error("Authentication failed! Retrying...")
                 raise Exception(f"Auth failed with status {response.status_code}")
         except Exception as e:
             retries += 1
-            print(f"[WARN] Attempt {retries}/{MAX_RETRIES} failed: {e}")
+            logger.warning(f"Attempt {retries}/{MAX_RETRIES} failed: {e}")
             if retries < MAX_RETRIES:
                 time.sleep(RETRY_DELAY)
             else:
-                print(f"[FATAL] Authentication failed after {MAX_RETRIES} attempts.")
+                logger.critical(f"Authentication failed after {MAX_RETRIES} attempts.")
                 sys.exit(1)
