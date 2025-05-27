22b4a02360ab562f9539880f397fc214c71e16ed
WSpamer <spamer24310@gmail.com>
2025-05-27 09:45:27 +0200

    Disabled logging for library use.

--- src/nviro_fetch/__init__.py
+++ src/nviro_fetch/__init__.py
@@ -1,11 +1,16 @@
+from loguru import logger
+
 # from nviro_fetch import auth, fetch, data  # noqa: F403
 from .auth import authenticate  # noqa: F401
+from .data import extract_readings, get_nviro_data, get_sensor_readings  # noqa: F401
 from .fetch import (
-    fetch_devices,
-    fetch_device_sensors,
-    fetch_sensor_readings,
+    fetch_device_sensors,  # noqa: F401
+    fetch_devices,  # noqa: F401
+    fetch_sensor_readings,  # noqa: F401
 )  # noqa: F401
-from .data import get_nviro_data, get_sensor_readings, extract_readings  # noqa: F401
+
+logger.disable("nviro_fetch")  # Disable default logger for nviro-fetch
+# logger.remove()  # Remove default logger
 
 
 def hello() -> str:
