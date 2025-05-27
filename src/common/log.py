5b878797d62bbd9623a5ed77c395062b5a045407
WSpamer <spamer24310@gmail.com>
2025-05-27 09:47:09 +0200

    Created config functions for logging

---
+++ src/common/log.py
@@ -0,0 +1,32 @@
+import sys
+
+from loguru import logger
+
+from common.env import env_debug, env_path
+
+
+def config_log_level(log_level="") -> str:
+    logger.remove()  # Remove default logger
+    if not log_level:
+        log_level = env_debug("log_level")
+    return log_level
+
+    # logger.add(sys.stderr, level=log_level)  # Add stderr logger for debug output
+
+
+def config_logger(log_level: str = "") -> None:
+    log_level = config_log_level(log_level)
+    logger.add(
+        sys.stderr,
+        level=log_level,
+    )
+
+
+def config_logfile(filename: str = "nviro_fetch.log") -> None:
+    log_path = env_path("log")
+    logger.add(
+        f"{log_path}/{filename}",
+        rotation="1 MB",
+        retention="7 days",
+        level=config_log_level(),
+    )
